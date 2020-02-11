94296e97db92f7f1c9d4c07c2ee0d1c797f51362
Abhay Maurya <maurya.abhay30@gmail.com>
2020-02-11 22:09:22 +0530

    Added Backgrounds and Fonts, Basic Post Generation Done

--- content.py
+++ content.py
@@ -1,31 +1,16 @@
-from PIL import Image,ImageDraw
-from random import randint as rint
- 
-def random_gradient(name):
-    img = Image.new("RGBA", (1080,1080), "#FFFFFF")
-    draw = ImageDraw.Draw(img)
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from random import randint
+import sys, os
 
-    #r,g,b = rint(0,255), rint(0,255), rint(0,255)
-    r,g,b = 253, 110, 106
-    #dr = (rint(0,255) - r)/500.
-    #dg = (rint(0,255) - g)/500.
-    #db = (rint(0,255) - b)/500.
-    dr,dg,db = 255/2000, 198/2000, 0/2000
-    for i in range(1580):
-        r,g,b = r+dr, g+dg, b+db
-        draw.line((0,i,1080,i-500), fill=(int(r),int(g),int(b)))
+x=randint(1,9)
+# path = os.path.abspath('.')
+bg_path = f"C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\BG\\{x}.png"
+post = Image.open(bg_path)
+draw = ImageDraw.Draw(post)
+font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 160)
+draw.text((0, 0),"Sample Text",(50,50,50),font=font)
+post.save(f"C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\post.png")
 
-    img.save(name+".png", "PNG")
-# img = Image.new('RGBA', (1080, 1080), color = (253, 110, 106))
-# img.save('test.png')
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-
-
-
-if __name__ == "__main__":
-    for name in range(1):
-        random_gradient(str(name))
+post.show()
