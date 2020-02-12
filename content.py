b30a6508979f2d72e74735ffc7663945e1b47cdf
Abhay Maurya <maurya.abhay30@gmail.com>
2020-02-12 19:17:12 +0530

    Changed text font

--- content.py
+++ content.py
@@ -48,10 +48,10 @@ draw = ImageDraw.Draw(post)
 font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 30)
 draw.text((810, 1020),"@liquiatorcoder",(50,50,50),font=font)
 
-font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 70)
+font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-SemiBoldItalic.ttf", 70)
 w, h = draw.textsize(text, font=font)
 print(w, h)
-draw.text(((W-w)/2,(H-h)/2), text, (70,70,70), font = font)
+draw.text(((W-w)/2,(H-h)/2), text, (30,30,30), font = font)
 
 post.save(f"C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\post.png")
 
