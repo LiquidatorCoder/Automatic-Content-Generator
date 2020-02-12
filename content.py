392cfb2476e543f7cea6908a2a8c64f8e35f38c4
Abhay Maurya <maurya.abhay30@gmail.com>
2020-02-12 19:11:45 +0530

    Added quote to image and added padding to quote

--- content.py
+++ content.py
@@ -16,9 +16,13 @@ for quote in quotes:
     text = quote.find('span', class_='text').text
     author = quote.find('small', class_='author').text
     scraped.append([text, author])
-qno = randint(0,9)
-quotel = scraped[qno][0]
-quotel = quotel.split(" ")
+quotel = []
+auth = ""
+while len(quotel)>25 or len(quotel)==0:
+    qno = randint(0,9)
+    quotel = scraped[qno][0]
+    quotel = quotel.split(" ")
+    auth = f"- {scraped[qno][1]}"
 quote = ""
 
 c=0
@@ -29,33 +33,31 @@ for i in quotel:
         c = 0
     else:
         quote = quote + i + " "
-auth = f"- {scraped[qno][1]}"
+quote = quote.replace('“','" ')
+quote = quote.replace('”',' "')
+
+
 print(quote)
 print(auth)
 x=randint(1,9)
 # path = os.path.abspath('.')
+
+W, H = (1080,1080)
 bg_path = f"C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\BG\\{x}.png"
 post = Image.open(bg_path)
 draw = ImageDraw.Draw(post)
+
 font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 30)
 draw.text((810, 1020),"@liquiatorcoder",(50,50,50),font=font)
 
-font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 60)
-draw.text((300, 300),quote,(70,70,70),font=font)
+font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 70)
+w, h = draw.textsize(quote, font=font)
+print(w, h)
+draw.text(((W-w)/2,(H-h)/2), quote, (70,70,70), font = font)
 
-font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 60)
-draw.text((500, 700),auth,(70,70,70),font=font)
+# font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 60)
+# draw.text((500, 700),auth,(70,70,70),font=font)
 
 post.save(f"C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\post.png")
 
 post.show()
-
-"""
-W, H = (1080,1080)
-
-
-font = ImageFont.truetype("C:\\Users\\Asus\\Desktop\\AutomaticContentCreator\\Font\\Montserrat-Medium.ttf", 60)
-w, h = draw.textsize(quote, font=Font)
-draw.text(((W-w)/2,(H-h)/2), msg, fill=(70,70,70))
-
-"""
