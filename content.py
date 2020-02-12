c0fede84d0d27ffefd9239e041063cdcd8991e12
Abhay Maurya <maurya.abhay30@gmail.com>
2020-02-12 21:39:38 +0530

    Updated comments

--- content.py
+++ content.py
@@ -1,3 +1,4 @@
+#-----------------------------
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -5,7 +6,7 @@ from random import randint
 import sys, os
 import requests
 from bs4 import BeautifulSoup
-
+#-----------------------------
 pno = randint(1,10)
 result = requests.get(f'http://quotes.toscrape.com/page/{pno}/')
 page = result.text
@@ -16,6 +17,7 @@ for quote in quotes:
     text = quote.find('span', class_='text').text
     author = quote.find('small', class_='author').text
     scraped.append([text, author])
+#-----------------------------
 quotel = []
 auth = ""
 while len(quotel)>25 or len(quotel)==0:
@@ -24,7 +26,6 @@ while len(quotel)>25 or len(quotel)==0:
     quotel = quotel.split(" ")
     auth = f"- {scraped[qno][1]}"
 quote = ""
-
 c=0
 for i in quotel:
     c+=1
@@ -35,23 +36,20 @@ for i in quotel:
         quote = quote + i + " "
 quote = quote.replace('“','" ')
 quote = quote.replace('”',' "')
-
 text = quote + "\n\n" + auth
+#-----------------------------
 x=randint(1,9)
-
 W, H = (1080,1080)
 bg_path = os.path.join(os.getcwd(),f"BG\\{x}.png")
 post = Image.open(bg_path)
 draw = ImageDraw.Draw(post)
-
+#-----------------------------
 font = ImageFont.truetype(os.path.join(os.getcwd(),"Font\\Montserrat-Medium.ttf"), 30)
 draw.text((810, 1020),"@liquiatorcoder",(50,50,50),font=font)
-
+#-----------------------------
 font = ImageFont.truetype(os.path.join(os.getcwd(),"Font\\Montserrat-SemiBoldItalic.ttf"), 70)
 w, h = draw.textsize(text, font=font)
-
 draw.text(((W-w)/2,(H-h)/2), text, (30,30,30), font = font)
-
+#-----------------------------
 post.save(os.path.join(os.getcwd(),"Post\\post.png"))
-
-post.show()
+#-----------------------------
